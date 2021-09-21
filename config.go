eb7dc1f85816189455e9397f9526e520880b2896
Rafael <rafael_breno@outlook.com>
2021-09-21 19:34:09 -0300

    feat: Add MaxTries field into Config struct

--- config.go
+++ config.go
@@ -6,10 +6,16 @@ type Config struct {
 	// OAuthToken is the token to allow you
 	// to connect to the IRC.
 	OAuthToken string
+
 	// BotUsername must be the same as the account
 	// that was used to generate the OAuthToken.
 	BotUsername string
+
 	// Channel is the name of a Twitch's channel
 	// that you want to your bot to connect to.
 	Channel string
+
+	// MaxTries set de amount of times between
+	// each connection retry.
+	MaxTries uint
 }
