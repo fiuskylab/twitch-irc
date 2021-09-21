9640466ad2239f89d6f3163aea1db9aff24fca4f
Rafael <rafael_breno@outlook.com>
2021-09-21 18:59:38 -0300

    feat: Add config struct

---
+++ config.go
@@ -0,0 +1,15 @@
+package twitchirc
+
+// Config store the base information
+// for a connection with Twitch's IRC.
+type Config struct {
+	// OAuthToken is the token to allow you
+	// to connect to the IRC.
+	OAuthToken string
+	// BotUsername must be the same as the account
+	// that was used to generate the OAuthToken.
+	BotUsername string
+	// Channel is the name of a Twitch's channel
+	// that you want to your bot to connect to.
+	Channel string
+}
