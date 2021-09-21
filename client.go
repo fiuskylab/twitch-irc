6f407e241ee58c535beac3eacd76622d1acb9c75
Rafael <rafael_breno@outlook.com>
2021-09-21 19:41:26 -0300

    feat: Add TCP connection function

--- client.go
+++ client.go
@@ -1,16 +1,45 @@
 package twitchirc
 
-import "net/textproto"
+import (
+	"net"
+	"net/textproto"
+	"time"
+)
 
 // Client is the IRC connector.
 type Client struct {
-	cfg *Config
-	tp  *textproto.Reader
+	cfg  *Config
+	tp   *textproto.Reader
+	conn *net.Conn
 }
+
+const (
+	IRC_URL = "irc://irc.chat.twitch.tv:6667"
+)
 
 // NewClient returns a Client instance
 func NewClient(cfg *Config) (*Client, error) {
 	c := Client{cfg: cfg}
 
+	if err := c.setTCPConn(); err != nil {
+		return c, err
+	}
+
 	return &c, nil
+}
+
+// setTCPConn dials and set TCP connection
+func (c *Client) setTCPConn() error {
+	var conn net.Conn
+	var err error
+
+	for i := uint(0); i < c.cfg.MaxTries; i++ {
+		conn, err = net.Dial("tcp", IRC_URL)
+		if err != nil {
+			time.Sleep(time.Second)
+		} else {
+			c.conn = &conn
+		}
+	}
+	return err
 }
